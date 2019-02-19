2b2e370185aae9001fd9b73ff7ab8f026ff6f58b
aukustik <aukustik@yandex.ru>
2019-02-19 15:40:50 +0700

    aukustik
    added on_wall anim

---
+++ Assets/Scripts/CamControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControls : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, 0f, -2f);
+    }
+}
